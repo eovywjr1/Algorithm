f005ec4ccbbbdbb4397d733dc25ed3defb0b2bff
eovywjr1 <eovywjr1@naver.com>
2024-07-08 20:07:43 +0900

    Create BOJ_5525.cpp

---
+++ sprint4/BOJ_5525.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	string str;
+	cin >> str;
+
+	int index = M - 1;
+	int answer = 0;
+
+	for (int index = 0; index < M; ++index) {
+		if (str[index] != 'I')
+			continue;
+
+		int ioiCount = 0;
+
+		while (str[index + 1] == 'O' && str[index + 2] == 'I') {
+			++ioiCount;
+
+			if (ioiCount == N) {
+				++answer;
+				--ioiCount;
+			}
+
+			index += 2;
+		}
+	}
+
+	cout << answer;
+}
