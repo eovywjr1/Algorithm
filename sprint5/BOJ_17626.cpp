d0b5d179fa649dd5966548c8e79d4bc8db6b656d
eovywjr1 <eovywjr1@naver.com>
2024-07-10 19:56:17 +0900

    Create BOJ_17626.cpp

---
+++ sprint5/BOJ_17626.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+#include <limits.h>
+
+using namespace std;
+
+bool IsInteger(const double num) {
+	if (num - static_cast<int>(num) == 0)
+		return true;
+
+	return false;
+}
+
+int check(const int n, const int remainNum, const int count) {
+	const int subNum = pow(n, 2);
+
+	if (count > 4 || subNum * (4 - count) < remainNum)
+		return INT_MAX - 1;
+
+	if (n == 1)
+		return remainNum;
+
+	if (remainNum <= 0)
+		return 0;
+	else if (n <= 0)
+		return INT_MAX - 1;
+
+	int tempCount = 1 + check(sqrt(remainNum - subNum), remainNum - subNum, count + 1);
+	tempCount = min(tempCount, check(n - 1, remainNum, count));
+
+	return tempCount;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	cout << check(sqrt(n), n, 0);
+	
+	return 0;
+}
