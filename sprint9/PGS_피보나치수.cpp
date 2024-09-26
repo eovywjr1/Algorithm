5100a9733cf7ce95ee7e0a5c0931142308866f6b
eovywjr1 <eovywjr1@naver.com>
2024-09-26 23:40:56 +0900

    Create PGS_피보나치수.cpp

---
+++ sprint9/PGS_피보나치수.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    vector<long long> fibonacciVector(n+1);
+    fibonacciVector[0]=0;
+    fibonacciVector[1]=1;
+    
+    for(int i=2;i<=n;++i){
+        fibonacciVector[i] = (fibonacciVector[i-1] + fibonacciVector[i-2]) % 1234567;
+    }
+    
+    return fibonacciVector[n];
+}
