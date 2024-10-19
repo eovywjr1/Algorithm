64ca5ce09d10fd966bf5715218a7f82b29389c72
eovywjr1 <eovywjr1@naver.com>
2024-10-20 02:00:17 +0900

    Create BOJ_1010_다리놓기_dp.cpp

---
+++ sprint10/BOJ_1010_다리놓기_dp.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int testCase = 0;
+	cin >> testCase;
+
+	constexpr int maxSiteCount = 30;
+	array<array<long long, maxSiteCount>, maxSiteCount> edgeCaseArray;
+
+	for (int siteCount = 1; siteCount < maxSiteCount; ++siteCount)
+	{
+		edgeCaseArray[1][siteCount] = siteCount;
+		edgeCaseArray[siteCount][1] = siteCount;
+	}
+
+	for (int leftSiteCount = 2; leftSiteCount < maxSiteCount; ++leftSiteCount)
+	{
+		for (int rightSiteCount = 2; rightSiteCount < maxSiteCount; ++rightSiteCount)
+		{
+			long long edgeCaseSum = 0;
+			
+			for (int subRightSiteCount = leftSiteCount - 1; subRightSiteCount < rightSiteCount; ++subRightSiteCount)
+			{
+				edgeCaseSum += edgeCaseArray[leftSiteCount - 1][subRightSiteCount];
+			}
+			
+			edgeCaseArray[leftSiteCount][rightSiteCount] = edgeCaseSum;
+		}
+	}
+
+	for (int testCount = 0; testCount < testCase; ++testCount)
+	{
+		int leftSite = 0, rightSite = 0;
+		cin >> leftSite >> rightSite;
+
+		cout << edgeCaseArray[leftSite][rightSite] << '\n';
+	}
+}
