c266a881b6246723b2226ad64d9e3623c395b16e
eovywjr1 <eovywjr1@naver.com>
2025-02-20 16:52:55 +0900

    Update BOJ_11404_플로이드.cpp

--- sprint12/BOJ_11404_플로이드.cpp
+++ sprint12/BOJ_11404_플로이드.cpp
@@ -9,9 +9,14 @@ int main()
     int n = 0, m = 0;
     cin >> n >> m;
 
-    const int invalidDistance = 100001;
+    const int invalidDistance = 100000 * 100 + 1;
 
     vector<vector<int>> distances(n, vector<int>(n, invalidDistance));
+
+    for (int index = 0; index < n; ++index)
+    {
+        distances[index][index] = 0;
+    }
 
     for (int index = 0; index < m; ++index)
     {
@@ -21,16 +26,11 @@ int main()
         distances[start - 1][end - 1] = min(distances[start - 1][end - 1], distance);
     }
 
-    for (int index = 0; index < n; ++index)
-    {
-        distances[index][index] = 0;
-    }
-
-    for (int k = 0; k < n; k++) // k 는 거쳐가는 정점
+    for (int k = 0; k < n; ++k) // k 는 거쳐가는 정점
     {  
-		for (int i = 0; i < n; i++) // i 는 행 (출발 정점)
+		for (int i = 0; i < n; ++i) // i 는 행 (출발 정점)
         {
-            for (int j = 0; j < n; j++) // j 는 열 (도착 정점)
+            for (int j = 0; j < n; ++j) // j 는 열 (도착 정점)
 			{
                 distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
             }
@@ -47,8 +47,10 @@ int main()
             }
             else
             {
-                cout << distances[i][j] << ' ';
+                cout << distances[i][j];
             }
+
+            cout << ' ';
         }
 
         cout << '\n';
