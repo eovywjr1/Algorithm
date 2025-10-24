2227ee809437b6eda6408c9aa3d0036dea7d0070
eovywjr1 <eovywjr1@naver.com>
2025-10-24 22:15:46 +0900

    Create PGS_예상대진표.cpp

---
+++ sprint15/PGS_예상대진표.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int answer = 0;
+    
+    while(a != b)
+    {
+        ++answer;
+        
+        a = (a + 1) >> 1;
+        b = (b + 1) >> 1;
+    }
+
+    return answer;
+}
